8b114a890af4b8bc2d36fa10d0bd57116ef2f257
Юманов Алексей Александрович <yumanov.aa@spb.local>
2017-04-11 13:03:58 +0300

    Исправлен скрипт мониторинга дисковой подистемы. Теперь мониторин использует промежуточный хэш для ускорения работы шаблона мониторинга zabbix.

---
+++ win_disk_monitor/scripts/discovery.ps1
@@ -0,0 +1,76 @@
+﻿###########################################################
+###########################################################
+####                                                   ####
+####             Скрипт для автоматического            ####
+####           обнаружения физических дисков           ####
+####                                                   ####
+###########################################################
+###########################################################
+
+#Configurations scritp#
+$ConfigFile = "C:\zabbix\scripts\win_disk_monitor\config\physical_disk_monitor_param.json"
+
+function CreateConfig ($ConfigFile) {
+    $PhysicalDiskParam = @()
+    $MetaParam  = (Get-Counter -ListSet PhysicalDisk).PathsWithInstances
+    foreach ($param in $MetaParam) {
+        $disk = [regex]::split($param, '\(|\)')
+        if (($disk[1] -notmatch "total") -and ($disk[2] -match "Current Disk Queue Length")){
+            $a = [regex]::split($disk[1], '\s')
+            $object = New-Object -TypeName PSObject
+            $object | Add-Member -Name 'NameDisk' -MemberType Noteproperty -Value $a[1][0]
+            $object | Add-Member -Name 'Enable' -MemberType Noteproperty -Value $FALSE
+            $object | Add-Member -Name 'Params' -MemberType Noteproperty -Value (New-Object Collections.ArrayList)
+            $PhysicalDiskParam += $object
+        }
+    }
+    foreach ($param in $MetaParam) {
+        $disk = [regex]::split($param, '\(|\)')
+        if ($disk[1] -notmatch "total"){
+            $a = [regex]::split($disk[1], '\s')
+            $object = New-Object -TypeName PSObject
+            $object | Add-Member -Name 'DeviseID' -MemberType Noteproperty -Value $a[0]
+            $object | Add-Member -Name 'DeviseLabel' -MemberType Noteproperty -Value $a[1][0]
+            $object | Add-Member -Name 'ShortCmdled' -MemberType Noteproperty -Value ($disk[2] -replace "\s|\.|%|\\|/","")
+            $object | Add-Member -Name 'PerfomanceCommand' -MemberType Noteproperty -Value $param
+            $object | Add-Member -Name 'Enable' -MemberType Noteproperty -Value $TRUE
+            $l = 0
+            foreach ($id in $PhysicalDiskParam) {
+                if ($id.NameDisk -eq $a[1][0]) {break} else {$l++}
+            }
+            $temp = $PhysicalDiskParam[$l].Params.Add($object)
+        }
+    }
+    $JSON_Response = "[`r`n` `t{`r`n"
+    $first=0
+    foreach ($param in $PhysicalDiskParam) {
+        if ($first -ne 0 ) { $JSON_Response = $JSON_Response + "`n`t},`n`t{`n" }
+        $JSON_Response = $JSON_Response + "`t`t`"NameDisk`": `"" + $param.NameDisk + "`",`n"
+        $JSON_Response = $JSON_Response + "`t`t`"Enable`": `"" + $param.Enable + "`",`n"
+        $JSON_Response = $JSON_Response + "`t`t`"Params`": `n" + (ConvertTo-Json $param.Params)
+        $first++
+    }
+    $JSON_Response = $JSON_Response + "`r`n`t}`r`n]"
+    $JSON_Response >> $ConfigFile
+}
+
+function CreateJSONDiscovery ($ConfigFile) {
+    $PhysicalDiskParam = Get-Content -RAW $ConfigFile | ConvertFrom-Json
+    $JSON_Response = "{`r`n` `t`"data`":[`r`n"
+    $first=0
+    ForEach ($param in $PhysicalDiskParam) {
+        if ($param.Enable -eq $TRUE) {
+            if ($first -ne 0 ) { $JSON_Response = $JSON_Response + ", `r`n" }
+            $JSON_Response = $JSON_Response + "`t`t{ `"{`#DEVICEID}`":`"" + $param.Params[0].DeviseID + "`", `"{#DISKID}`":`"" + $param.Params[0].DeviseLabel +"`" }"
+            $first++
+        }
+    }
+    $JSON_Response = $JSON_Response + "`r`n`t]`r`n}"
+    return $JSON_Response
+}
+
+if (!(Test-Path $ConfigFile)) {
+    CreateConfig $ConfigFile
+} else {
+    CreateJSONDiscovery $ConfigFile
+}
---
+++ win_disk_monitor/scripts/param.ps1
@@ -0,0 +1,31 @@
+﻿###########################################################
+###########################################################
+####                                                   ####
+####             Скрипт для обновления                 ####
+####         элементов данных производительности       ####
+####              дисковой подсистемы                  ####
+####                                                   ####
+###########################################################
+###########################################################
+
+$watch = [System.Diagnostics.Stopwatch]::StartNew()
+
+$ConfigFile = "C:\zabbix\scripts\win_disk_monitor\config\physical_disk_monitor_param.json"
+$CacheFolder = "C:\zabbix\scripts\win_disk_monitor\cache"
+
+$rezult = New-Object Collections.ArrayList
+
+$PhysicalDiskParam = Get-Content -RAW $ConfigFile | ConvertFrom-Json
+ForEach ($param in $PhysicalDiskParam) {
+    if ($param.Enable -eq $TRUE) {
+        ForEach ($value in $param.Params) {
+            if ($value.Enable -eq $TRUE) {
+                $temp = (Get-Counter -Counter ($value.PerfomanceCommand)).CounterSamples.CookedValue
+                $pathcache = $CacheFolder + "\" + $value.DeviseID + "_" + $value.DeviseLabel + "_" + $value.ShortCmdled
+                [math]::Round($temp, 2) -replace ",","." > $pathcache
+            }
+        }
+    }
+}
+$watch.Stop()
+$watch.Elapsed.Seconds > ($CacheFolder + "\execution_time")
